3d04ae2e647913f3b90e469e86f7feedb59ae444
Olivier Piras <github@oprs.eu>
2011-04-23 19:13:06 +0200

    regex-c: improved retool_dfa

--- regex-c/retool_dfa.c
+++ regex-c/retool_dfa.c
@@ -36,6 +36,11 @@
 int
 retool_dfa( int argc, char* argv[] )
 {
+   atom_t dv = nil;
+   atom_t iv;
+   atom_t d;
+   int i, p;
+
    if( argc < 3 ) {
       (void)fprintf( stderr, "usage: dfa regex alphabet\n" );
       return 1;
@@ -44,11 +49,43 @@ retool_dfa( int argc, char* argv[] )
    atom_t atom = re_posix_parse( argv[1] );
    char *x = argv[2];
 
+   (void)printf( "\n" );
+   (void)printf( "      " );
    while( *x ) {
-      (void)printf( "'%c': ", *x );
-      re_posix_dump( brz_deriv( atom, *x ) );
+      (void)printf( "   %c", *x );
       ++x;
    }
+   (void)printf( "\n\n" );
+
+   dv = list2( nil, atom );
+   iv = cdr(dv);
+
+   for( i = 1 ; !nullp(iv) ; ++i ) {
+      (void)printf( "% 4d: ", i );
+      x = argv[2];
+      while( *x ) {
+         d = brz_deriv( atom, *x );
+         p = position( d, dv );
+         if( p < 0 )
+            dv = append( dv, d );
+         p = position( d, dv );
+         if( p ) {
+            (void)printf( "% 4d", p );
+         } else {
+            (void)printf( "   ." );
+         }
+         ++x;
+      }
+
+      (void)printf( "    " );
+      re_posix_dump( atom );
+      iv = cdr(iv);
+      if( nullp(iv) )
+         break;
+      atom = car(iv);
+   }
+
+   (void)printf( "\n" );
 
    return !truep( atom );
 }
