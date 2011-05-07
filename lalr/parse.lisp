62efae6c26ded19d8b392958cb1b066e56d78324
Olivier Piras <github@oprs.eu>
2011-05-07 18:24:22 +0200

    lalr: introduced tokens instead of symbols

--- lalr/parse.lisp
+++ lalr/parse.lisp
@@ -1,13 +1,13 @@
 
-(defparameter *symbols* (make-hash-table))
+(defparameter *tokens* (make-hash-table))
 
-(defmacro define (label args)
- `(setf (gethash ',label *symbols*) ',args))
+(defmacro token (label args)
+ `(setf (gethash ',label *tokens*) ',args))
 
-(load (merge-pathnames "c-grammar" *load-truename*))
+;(load (merge-pathnames "c-grammar" *load-truename*))
 
-(defun dump-symbol (atom)
-  (let ((val (gethash atom *symbols*)))
+(defun dump-token (atom)
+  (let ((val (gethash atom *tokens*)))
 ;(format t "~s: ~s~%" atom val)
       (if (null val)
         (error "undefined lexem ~s" atom)
@@ -15,7 +15,7 @@
 
 (defun dump (atom)
   (cond
-    ((symbolp atom) (dump-symbol atom))
+    ((symbolp atom) (dump-token atom))
     ((consp   atom) (nconc (cons (car atom) nil)
                            (mapcar #'dump (cdr atom))))
     (t        atom)))
